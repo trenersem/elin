4281c878f23aab7e97b9875920ebb283c1b3db5f
Semenov Volodymyr <trenersem1998@gmail.com>
2024-06-26 22:17:57 -0400

    fix cache data

--- services/availability/getAvailabilityByDate.ts
+++ services/availability/getAvailabilityByDate.ts
@@ -1,3 +1,5 @@
+export const fetchCache = 'force-no-store';
+
 export const getAvailabilityByDate = async (date: string): Promise<any> => {
      const response = await fetch(`/api/availability/${date}`, { cache: 'no-store' });
      if (!response.ok) {
