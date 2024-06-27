3e94f5f56bbefb0fd00fa0edb95e130decc5c7d3
Semenov Volodymyr <trenersem1998@gmail.com>
2024-06-26 22:46:51 -0400

    fix cache data

--- services/fetchAppointments.ts
+++ services/fetchAppointments.ts
@@ -1,13 +1,10 @@
+export const fetchCache = 'force-no-store';
+
 import { IAppointment } from "@/types";
 
 export const fetchAppointments = async (): Promise<IAppointment[]> => {
   const response = await fetch('/api/appointments', { 
-    cache: 'no-store',
-    headers: {
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-      'Pragma': 'no-cache',
-      'Expires': '0'
-    }
+    cache: 'no-store'
   });
   if (!response.ok) {
     throw new Error('Failed to fetch appointments');
