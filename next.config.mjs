5dbf2adbb90483f24d04b73cf2e5d10a1060952d
Semenov Volodymyr <trenersem1998@gmail.com>
2024-06-27 00:38:11 -0400

    fix cache data

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     output: 'export',
+    distDir: 'build',
+//      experimental: {
+//          appDir: true,
+//          serverComponentsExternalPackages: ["mongoose"],
+//      },
+//      output: 'export',
+//      webpack(config) {
+//          config.experiments = {
+//         ...config.experiments,
+//         topLevelAwait: true,
+//         }
+//     return config
+//   }
 };
 
 export default nextConfig;
